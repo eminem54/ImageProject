95a65c344f2317cea0eb57a34d5c98b0e78b1b8c
eminem54 <eminem54@naver.com>
2018-10-27 03:29:15 +0900

    bit slice

--- main.cpp
+++ main.cpp
@@ -1,47 +1,96 @@
-//#include "Utility.h";
-//#include <iostream>
-//#include <opencv2\opencv.hpp>
-//using namespace std;
-//using namespace cv;
-//
-//
-//int main() {
-//	const char* inputPath = "face1.bmp";
-//
-//	unsigned char* pixels = ReadBMP(inputPath);
-//
-//	FILE* f = fopen(inputPath, "rb");
-//	if (f == NULL)
-//		throw "Argument Exception";
-//
-//	unsigned char info[54];
-//	fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
-//
-//											   // extract image height and width from header
-//	int nWidth_in = *(int*)&info[18];
-//	int nHeight_in = *(int*)&info[22];
-//	fclose(f);
-//
-//	unsigned char** ch_in_R = allocMem(nHeight_in, nWidth_in, 0); 
-//	unsigned char** ch_in_G = allocMem(nHeight_in, nWidth_in, 0); 
-//	unsigned char** ch_in_B = allocMem(nHeight_in, nWidth_in, 0); 
-//
-//	for (int i = 0; i < nHeight_in; i++) {
-//		for (int j = 0; j < nWidth_in * 3; j += 3) {
-//			ch_in_R[i][j / 3] = pixels[j];
-//			ch_in_G[i][j / 3] = pixels[j+1];
-//			ch_in_B[i][j / 3] = pixels[j+2];
-//		}
-//	}
-//
-//	Mat result = Mat(nHeight_in, nWidth_in, CV_8UC3);
-//	for (int h = 0; h < nHeight_in; h++) {
-//		for (int w = 0; w < nWidth_in; w++) {
-//			result.at<Vec3b>(h, w) = Vec3b(ch_in_B[h][w], ch_in_G[h][w], ch_in_R[h][w]);
-//		}
-//	}
-//
-//	imshow("result", result);
-//	waitKey(0);
-//	return 0;
-//}
+#include "Utility.h";
+#include <iostream>
+#include <opencv2\opencv.hpp>
+using namespace std;
+using namespace cv;
+
+void getBitPlane(Mat &srcImg, Mat &dstImg, int n)
+{
+	uchar mask = 0x01 << n;
+	Mat s = Mat(srcImg);
+	Mat d = Mat(dstImg);
+
+	for (int r = 0; r < srcImg.rows; r++)
+	{
+		uchar *p = srcImg.ptr(r);//첫번째행의 값을 가져온다.
+		uchar *dp = dstImg.ptr(r);
+
+		for (int c = 0; c < srcImg.cols; c++)
+		{
+			uchar pixelvalue = p[c];
+			uchar one_zero = pixelvalue & mask;
+			if (one_zero > 0)
+				dp[c] = 255;
+			else
+				dp[c] = 0;
+		}
+	}
+}
+
+int main() {
+	const char* inputPath = "face1.bmp";
+	Mat Ori_Img = imread(inputPath);
+
+	int nHeight_in = Ori_Img.rows;
+	int nWidth_in= Ori_Img.cols;
+
+	char mask = 0x01 << 7;
+	char mask2 = 0x01 << 4;
+	
+	unsigned char** ch_in_R;
+	unsigned char** ch_in_G;
+	unsigned char** ch_in_B;
+
+	if (Ori_Img.channels() == 3) {
+		ch_in_R = allocMem(nHeight_in, nWidth_in, 0);
+		ch_in_G = allocMem(nHeight_in, nWidth_in, 0);
+		ch_in_B = allocMem(nHeight_in, nWidth_in, 0);
+
+		for (int h = 0; h < nHeight_in; h++) {
+			for (int w = 0; w < nWidth_in; w++) {
+				Vec3b RGB = Ori_Img.at<Vec3b>(h, w);
+				ch_in_R[h][w] = (unsigned char)RGB[2];
+				ch_in_G[h][w] = (unsigned char)RGB[1];
+				ch_in_B[h][w] = (unsigned char)RGB[0];
+			}
+		}
+	}
+
+	Mat result = Mat(nHeight_in, nWidth_in, CV_8UC3);
+	for (int h = 0; h < nHeight_in; h++) {
+		uchar *or = ch_in_R[h];
+		uchar *og = ch_in_G[h];
+		uchar *ob = ch_in_B[h];
+
+		uchar *dr = ch_in_B[h];
+		uchar *dg = ch_in_B[h];
+		uchar *db = ch_in_B[h];
+
+
+		for (int w = 0; w < nWidth_in; w++) {
+				uchar pixelR = or[w];
+				uchar pixelG = og[w];
+				uchar pixelB = ob[w];
+
+				uchar afterR = pixelR & mask;
+				uchar afterG = pixelG & mask;
+				uchar afterB = pixelB & mask;
+
+				if (afterR> 0) afterR = 255;
+				else afterR = 0;
+				if (afterG> 0) afterG = 255;
+				else afterG = 0;
+				if (afterB> 0) afterB = 255;
+				else afterB = 0;
+
+				result.at<Vec3b>(h, w) = Vec3b(afterB, ch_in_G[h][w], afterR);
+		}
+	}
+
+	//getBitPlane(Ori_Img, result, 6);
+
+	imshow("hi", result);
+	waitKey(0);
+
+	return 0;
+}
